22e2ed4f9f259f1ad37a01cbcab9c564c59b1f8a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:01 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "mssql_managed_databases" {
-  description = "All mssql_managed_database resources"
-  value       = azurerm_mssql_managed_database.mssql_managed_databases
-}
 output "mssql_managed_databases_long_term_retention_policy" {
-  description = "List of long_term_retention_policy values across all mssql_managed_databases"
-  value       = [for k, v in azurerm_mssql_managed_database.mssql_managed_databases : v.long_term_retention_policy]
+  description = "Map of long_term_retention_policy values across all mssql_managed_databases, keyed the same as var.mssql_managed_databases"
+  value       = { for k, v in azurerm_mssql_managed_database.mssql_managed_databases : k => v.long_term_retention_policy }
 }
 output "mssql_managed_databases_managed_instance_id" {
-  description = "List of managed_instance_id values across all mssql_managed_databases"
-  value       = [for k, v in azurerm_mssql_managed_database.mssql_managed_databases : v.managed_instance_id]
+  description = "Map of managed_instance_id values across all mssql_managed_databases, keyed the same as var.mssql_managed_databases"
+  value       = { for k, v in azurerm_mssql_managed_database.mssql_managed_databases : k => v.managed_instance_id }
 }
 output "mssql_managed_databases_name" {
-  description = "List of name values across all mssql_managed_databases"
-  value       = [for k, v in azurerm_mssql_managed_database.mssql_managed_databases : v.name]
+  description = "Map of name values across all mssql_managed_databases, keyed the same as var.mssql_managed_databases"
+  value       = { for k, v in azurerm_mssql_managed_database.mssql_managed_databases : k => v.name }
 }
 output "mssql_managed_databases_point_in_time_restore" {
-  description = "List of point_in_time_restore values across all mssql_managed_databases"
-  value       = [for k, v in azurerm_mssql_managed_database.mssql_managed_databases : v.point_in_time_restore]
+  description = "Map of point_in_time_restore values across all mssql_managed_databases, keyed the same as var.mssql_managed_databases"
+  value       = { for k, v in azurerm_mssql_managed_database.mssql_managed_databases : k => v.point_in_time_restore }
 }
 output "mssql_managed_databases_short_term_retention_days" {
-  description = "List of short_term_retention_days values across all mssql_managed_databases"
-  value       = [for k, v in azurerm_mssql_managed_database.mssql_managed_databases : v.short_term_retention_days]
+  description = "Map of short_term_retention_days values across all mssql_managed_databases, keyed the same as var.mssql_managed_databases"
+  value       = { for k, v in azurerm_mssql_managed_database.mssql_managed_databases : k => v.short_term_retention_days }
 }
 output "mssql_managed_databases_tags" {
-  description = "List of tags values across all mssql_managed_databases"
-  value       = [for k, v in azurerm_mssql_managed_database.mssql_managed_databases : v.tags]
+  description = "Map of tags values across all mssql_managed_databases, keyed the same as var.mssql_managed_databases"
+  value       = { for k, v in azurerm_mssql_managed_database.mssql_managed_databases : k => v.tags }
 }
 
